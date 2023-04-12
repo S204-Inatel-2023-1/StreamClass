5b3ec05111393e6b4512abdcd1934a408baa6ab8
Pedro Augusto <pedrodaluzmota208@gmail.com>
2023-04-12 18:43:30 -0300

    Front - Tela de Login

--- App/streamclass/lib/controller/backEnd.dart
+++ App/streamclass/lib/controller/backEnd.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 class BackEnd {
   Future<http.Response> buttonPressed() async {
-    final response = await http.get(Uri.parse("uri"),
+    final response = await http.get(Uri.parse("http//:localhost"),
     headers: <String, String>{
       'Content-Type': 'application/json; charset-UTF-8'
     });
